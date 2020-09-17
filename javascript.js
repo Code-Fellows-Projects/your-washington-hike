1962bc984cd84045496ba6b899eb18833066e50e
Stacy Burris <stacy1burris@gmail.com>
2020-09-17 12:38:03 -0700

    javascript addings

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
-var yourName = prompt('Please enter your name?');
-var greeting;
+var yourName = "Hello fellow hiker " + prompt("What's your name?") + "!";
+var greeting; 
+var whereFrom = prompt('Are you from Washington?');
+    
+
 
 if (yourName == 'Stacy'){
-    greeting = 'Welcome Majesty ' + yourName;
+    greeting = 'Welcome  ' + yourName;
 } else {
     greeting = 'Welcome ' + yourName;
 }
@@ -10,15 +13,24 @@ var today = new Date();
 var hourNow = today.getHours();
 
 if (hourNow > 18) {
-    greeting = 'Good evening!';
+    greeting = 'Good evening fellow hiker!';
 } else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
+    greeting = 'Good afternoon fellow hiker!';
 } else if (hourNow > 0 ) {
-    greeting = 'Good morning!';
+    greeting = 'Good morning fellow hiker!';
 } else {
     greeting = 'Welcome!';
 }
+
+
 document.write(greeting);
 
+if (confirm("Do you want to hike in Western or Eastern Washington?")) {
+    say = "Great! Lets find you a trail!";
+    } else {
+        say = "No problem, just browsing...";
+
+    }
+document.write(say);
 
 
